cd813fddea5bf0c93c684208836046d9e72a241e
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-14 22:42:56 +0200

    Add rendering children in main

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,4 +6,4 @@ import css from './styles/style.styl';
 import Main from './components/Main';
 
 
-render(<Main/>, document.getElementById('root'));
+render(<Main />, document.getElementById('root'));
