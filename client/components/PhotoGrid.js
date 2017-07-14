ef2ea0d5cc6bebcbb8d9f07ff8b4131f3b71934e
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-14 22:38:13 +0200

    Create photogrid and single components

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class PhotoGrid extends React.Component {
+  render () {
+    return (
+      <div className="photo-grid">
+        PhotoGrid over here
+      </div>
+    )
+  }
+}
+
+export default PhotoGrid;
