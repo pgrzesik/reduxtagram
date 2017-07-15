23b86dc596deb62d3df35caab935e561cfa705dd
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-15 12:35:25 +0200

    Wire up store to the application

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 import css from './styles/style.styl';
 
@@ -10,12 +12,14 @@ import PhotoGrid from './components/PhotoGrid';
 
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
