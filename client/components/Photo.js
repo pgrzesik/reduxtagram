58400f133e1998e6d0146c4a9e4676fb0fb4eca4
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-16 21:13:27 +0200

    Render Photos as links to detailed views

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 class Photo extends React.Component {
   render () {
+    const post = this.props.post;
     return (
       <figure className="grid-figure">
-        Photo here!
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            {post.caption}
+          </Link>
+        </div>
       </figure>
     )
   }
