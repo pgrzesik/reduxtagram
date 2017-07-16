c381a599e93566ece3cab7ce7f620e9ebef9b9d6
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-16 21:03:04 +0200

    Add a photo component

---
+++ client/components/Photo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Photo extends React.Component {
+  render () {
+    return (
+      <figure className="grid-figure">
+        Photo here!
+      </figure>
+    )
+  }
+}
+
+export default Photo;
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -5,7 +5,7 @@ class PhotoGrid extends React.Component {
   render () {
     return (
       <div className="photo-grid">
-        PhotoGrid over here
+        {JSON.stringify(this.props.posts)}
       </div>
     )
   }
