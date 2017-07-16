d6b8920dfa608be7e3ee765f559cb671a965098d
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-16 21:10:03 +0200

    Pass necessary props to Photo from PhotoGrid

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -6,7 +6,8 @@ class PhotoGrid extends React.Component {
   render () {
     return (
       <div className="photo-grid">
-        {this.props.posts.map((post, i) => <Photo />)}
+        {this.props.posts.map(
+          (post, i) => <Photo {...this.props} key={i} i={i} post={post}/>)}
       </div>
     )
   }
