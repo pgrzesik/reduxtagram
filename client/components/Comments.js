07ebd8a271204016f9dfaa5d706898549652eb80
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-17 18:25:02 +0200

    Actually display comments via Comments component

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class Comments extends React.Component {
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )  
+  }
+
   render () {
-    <div className="comment">
-      Hello from comment!
-    </div>
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentsForm" className="comment-form">
+          <input type="text" ref="author" placeholder="author"/>
+          <input type="text" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden/>
+        </form>
+      </div>
+    )
   }
 }
 
