e98c29ef674e66b49175a0299d756edff1bbc5a3
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-17 18:10:50 +0200

    Add empty comments component

---
+++ client/components/Comments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Comments extends React.Component {
+  render () {
+    <div className="comment">
+      Hello from comment!
+    </div>
+  }
+}
+
+export default Comments;
