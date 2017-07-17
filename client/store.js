f532dcae3b1dd286cdda3a314b6737807b1bd212
Piotr Grzesik <pj.grzesik@gmail.com>
2017-07-17 22:04:38 +0200

    Add hot reloading for reducers

--- client/store.js
+++ client/store.js
@@ -15,4 +15,11 @@ const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 export default store;
